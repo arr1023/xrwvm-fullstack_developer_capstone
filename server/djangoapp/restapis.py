c1ef8f52df4b93897b71a88af440895b9ba2d38f
arr1023 <168494962+arr1023@users.noreply.github.com>
2024-12-17 15:32:07 -0500

    s

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -27,7 +27,7 @@ def get_request(endpoint, **kwargs):
         return response.json()
     except Exception as e:
         # If any error occurs
-        print("Network exception occurred": {e})
+        print("Network exception occurred: {e}")
 
 
 def analyze_review_sentiments(text):
