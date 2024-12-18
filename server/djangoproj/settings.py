99d5eec12df2dd8070f33538213b22aba4d0d8fd
arr1023 <168494962+arr1023@users.noreply.github.com>
2024-12-18 01:57:00 -0500

    s

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -34,9 +34,9 @@ ALLOWED_HOSTS = [
     '-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
     ]
 CSRF_TRUSTED_ORIGINS = [
-    'https://alexisramire-8000.theiadockernext-1-labs' + 
-    '-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
-    ]
+    "https://alexisramire-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01."
+    "proxy.cognitiveclass.ai"
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -101,7 +101,7 @@ DATABASES = {
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'django.contrib.auth.password_validation.' 'UserAttributeSimilarityValidator',
     },
     {
         'NAME':
